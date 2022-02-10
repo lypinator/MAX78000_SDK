9901251ebbe969a71e2e06501ff15732ba01f3e1
Jake Carter <jake.carter@maximintegrated.com>
2022-02-09 22:10:17 -0600

    Update setup.sh script to improve compatability with RPi

--- setup.sh
+++ setup.sh
@@ -1,34 +1,114 @@
-echo "\nMAX78000_SDK auto-setup script\n"
+# VSCode-Maxim setup script.  **Run as user with non-elevated privileges**, ex:  bash setup.sh
 
-echo "Installing packages..."
-# Install packages
-apt update
-apt upgrade
-apt install --yes make libncurses5 libusb-1.0-0 libusb-0.1-4 libhidapi-libusb0 libhidapi-hidraw0
+SCRIPTDIR=$(cd $(dirname $0) && pwd)
+echo "Running setup.sh from $SCRIPTDIR"
 
-# Get user's home directory
-HOME_DIR=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6) # This should work if this script is run with sudo or normally
-echo "Located user home directory $HOME_DIR"
+ENV_FILE=/etc/profile.d/maximsdk-env.sh
 
-# Get location of script
-BASEDIR=$(dirname $0)
-MAXIM_PATH="$(cd $BASEDIR && pwd)"
-
-# Add MAXIM_PATH to shell startup script
-if [ -z "$(grep "export MAXIM_PATH=" $HOME_DIR/.bashrc)" ]; # If grep search for MAXIM_PATH in ~/.bashrc returns empty string (-z)
-then
-	echo "Setting MAXIM_PATH=$MAXIM_PATH in $HOME_DIR/.bashrc"
-	sed -i~ "1iexport MAXIM_PATH=${MAXIM_PATH}" $HOME_DIR/.bashrc # Insert line at beginning (1i) and create backup (-i~)
+# Check for existence of environment variable shell script on startup so we can refresh if needed
+if [ -f $ENV_FILE ]; then
+	source $ENV_FILE
 else
-	# Update MAXIM_PATH instead of adding new line
-	echo "$HOME_DIR/.bashrc already sets MAXIM_PATH.  Updating to MAXIM_PATH=$MAXIM_PATH just in case"
-	sed -i~ "s:.*export MAXIM_PATH=.*:export MAXIM_PATH=$MAXIM_PATH:" $HOME_DIR/.bashrc
+	echo "Failed to find /etc/profile.d/maximsdk-env.sh"
 fi
 
-# Copy OpenOCD rules file
-echo "Copying OpenOCD .rules file to /etc/udev/rules.d/"
-cp $MAXIM_PATH/Tools/OpenOCD/60-openocd.rules /etc/udev/rules.d/
-udevadm control --reload-rules
-udevadm trigger --attr-match=subsystem=net
+VERIFICATION_DIR=Libraries/CMSIS/Device/Maxim
+if [ ! -d ./$VERIFICATION_DIR ]; then
+	# Running script outside of MaximSDK
+	echo "This script is not located inside the root directory of a MaximSDK installation..."
+	
+	if [ -z "$MAXIM_PATH" ]; then
+		# No environment variable.  Collect path manually?
+		echo "Failed to locate MAXIM_PATH environment variable..."
+		echo "Please set MAXIM_PATH or place this script in the root directory of the MaximSDK installation and try again."
+		read -p "Alternatively, enter the MaximSDK installation path manually now? (y/n):" YN
+		if [ "$YN" = "y" ] || [ "$YN" = "Y" ]; then
+			read -p "Enter the full path of the MaximSDK installation (ex: /home/username/MaximSDK):" MAXIM_PATH
+			echo "Verifying..."
+		else
+			echo "Quitting..."
+			exit -1
+		fi
+	else
+		# Verify current environment variable is correct
+		echo "Located $MAXIM_PATH"
+		read -p "Is this the correct location of the latest MaximSDK installation? (y/n):" YN
+		if [ "$YN" = "y" ] || [ "$YN" = "Y" ]; then
+			echo "Verifying..."
+		elif [ "$YN" = "n" ] || [ "$YN" = "N" ]; then	
+			read -p "Enter the full path of the latest MaximSDK installation (ex: /home/username/MaximSDK):" MAXIM_PATH
+			echo "Verifying..."
+		else
+			exit -1
+		fi
+	fi
+else
+	# Running from inside MaximSDK.  Set MAXIM_PATH to location of this script
+	MAXIM_PATH=$SCRIPTDIR
+fi
 
-# TODO: Call user-side VS Code generator script?
+# Final verify
+if [ ! -d $MAXIM_PATH/$VERIFICATION_DIR ]; then
+	echo "Verification of MaximSDK installation failed.  Failed to find expected files."
+	exit -2
+else
+	echo "Verified SDK location!"
+fi
+
+# Add MAXIM_PATH to system environment variables
+echo "Adding MAXIM_PATH environment variable..."
+if [ -f $ENV_FILE ]; then
+	# Update existing file
+	echo "Updating $ENV_FILE ..."
+	if [ -z "$(sudo grep "export MAXIM_PATH=" $ENV_FILE)" ]; then
+		# If grep search for MAXIM_PATH returns empty string (-z)
+		echo "Setting MAXIM_PATH=$MAXIM_PATH"
+		sudo sed -i~ "1iexport MAXIM_PATH=${MAXIM_PATH}" $ENV_FILE # Insert line at beginning (1i) and create backup (-i~)
+	else
+		# Update MAXIM_PATH instead of adding new line
+		echo "$ENV_FILE already sets MAXIM_PATH.  Updating to use MAXIM_PATH=$MAXIM_PATH"
+		sudo sed -i~ "s:.*export MAXIM_PATH=.*:export MAXIM_PATH=$MAXIM_PATH:" $ENV_FILE # search and replace (s):pattern (.*=all):with this:
+	fi
+
+	echo "Reloading shell..."
+	source $ENV_FILE
+else
+	# Create new file
+	sudo touch $ENV_FILE
+	sudo sed -i "1iexport MAXIM_PATH=${MAXIM_PATH}" $ENV_FILE
+fi
+
+# Run updates.sh
+if [ -f $MAXIM_PATH/updates.sh ]; then
+	echo "Checking for updates..."
+	cd $MAXIM_PATH && bash updates.sh
+	cd $SCRIPTDIR
+fi
+
+
+# Get newer GCC
+GCC_VERSION="10.3.1"
+ARCH=$(arch)
+PACKAGE_STRING="10.3-2021.10"
+GCC_PATH="$MAXIM_PATH/Tools/GNUTools/gcc-arm-none-eabi-$GCC_VERSION"
+ARM_LINK="https://developer.arm.com/-/media/Files/downloads/gnu-rm/$PACKAGE_STRING/gcc-arm-none-eabi-$PACKAGE_STRING-$ARCH-linux.tar.bz2"
+
+echo "Getting Arm embedded toolchain v$GCC_VERSION from $ARM_LINK..."
+if [ ! -d $GCC_PATH ]; then
+	wget -O gcc-arm.tar.bz2 $ARM_LINK
+	mkdir -p $GCC_PATH
+	tar -xvf gcc-arm.tar.bz2 --directory $MAXIM_PATH/Tools/GNUTools/
+	cp -rf $MAXIM_PATH/Tools/GNUTools/$PACKAGE_NAME/* $GCC_PATH/
+	echo "Cleaning up..."
+	rm -rf $MAXIM_PATH/Tools/GNUTools/$PACKAGE_NAME
+	rm gcc-arm.tar.bz2
+else
+	echo "Already have $GCC_PATH"
+fi
+
+# Generate VS Code project files
+# Commented out until generator binaries are created for the RPi
+#echo "Generating VS Code project files..."
+#./generate SDK
+
+echo "Done!"
